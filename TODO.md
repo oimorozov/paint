0201dd8b753802376fe0ab85f40fc1c9557ba789
oimorozov <imorozovolejka@gmail.com>
2026-06-24 14:11:13 +0300

    Update move cmd line to bottom

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+---
+
+- Move command line to the bottom, and it make its size fully scaling depending on window size
--- main.c
+++ main.c
@@ -5,18 +5,23 @@
 #include <stdbool.h>
 #include "raylib.h"
 
-
 #define WINDOW_SIZE 800
 #define BRUSH_SIZE 5
 #define CMD_CAP 128
 
-#define CMD_LINE_W WINDOW_SIZE * 0.9f
-#define CMD_LINE_H WINDOW_SIZE * 0.3f
-#define CMD_LINE_X (float)WINDOW_SIZE / 2 - CMD_LINE_W / 2
-#define CMD_LINE_Y (float)WINDOW_SIZE / 2 - CMD_LINE_H / 2
+#define FONT_SIZE fmin(16, WINDOW_SIZE / 2)
 
+#define CMD_LINE_W WINDOW_SIZE
+#define CMD_LINE_H fmin(20, WINDOW_SIZE / 2)
+#define CMD_LINE_Y WINDOW_SIZE - CMD_LINE_H
 
-struct Config {
+#define BACKGROUND_COLOR DARKGRAY
+#define BRUSH_COLOR WHITE
+#define CMD_LINE_COLOR LIGHTGRAY
+#define FONT_COLOR BLACK
+
+struct Config
+{
     RenderTexture2D canvas;
 
     Vector2 curr_pos;
@@ -31,7 +36,8 @@ struct Config {
 typedef struct Config Config;
 
 /* returns 1 if succes, else 0 */
-int InitConfig(Config *config) {
+int InitConfig(Config *config)
+{
     // window
     InitWindow(WINDOW_SIZE, WINDOW_SIZE, "paint");
     SetTargetFPS(60);
@@ -54,7 +60,8 @@ int InitConfig(Config *config) {
     return 1;
 }
 
-void DestroyConfig(Config *config) {
+void DestroyConfig(Config *config)
+{
     // canvas
     UnloadRenderTexture(config->canvas);
 
@@ -62,88 +69,110 @@ void DestroyConfig(Config *config) {
     CloseWindow();
 }
 
-void DrawLineVec(Vector2 start_pos, Vector2 end_pos, int brush_size, Color color) {
+void DrawLineVec(Vector2 start_pos, Vector2 end_pos, int brush_size, Color color)
+{
     float dx = end_pos.x - start_pos.x;
     float dy = end_pos.y - start_pos.y;
     float dist = sqrtf(dx * dx + dy * dy);
-    if (dist == 0) {
+    if (dist == 0)
+    {
         DrawCircleV(start_pos, brush_size, color);
         return;
     }
 
     Vector2 dir = {dx / dist, dy / dist}; // {cos(a), sin(a)}
-    double step = brush_size;
-    for (float delta = 0.0f; delta <= dist; delta += step) {
+    double step = brush_size * 0.5f;
+    for (float delta = 0.0f; delta <= dist; delta += step)
+    {
         Vector2 pos = {start_pos.x + dir.x * delta, start_pos.y + dir.y * delta};
         DrawCircleV(pos, brush_size, color);
     }
 }
 
-void execute_command(Config *config, const char *cmd) {
+void execute_command(Config *config, const char *cmd)
+{
     char name[CMD_CAP / 2] = {0};
     char args[CMD_CAP / 2] = {0};
 
     sscanf(cmd, "%s %s", name, args);
 
-    if (strcmp(name, ":b") == 0) {
+    if (strcmp(name, ":b") == 0)
+    {
         int size = atoi(args);
 
-        if (0 < size && size < 101) {
+        if (0 < size && size < 101)
+        {
             config->brush_size = size;
         }
-    } else if (strcmp(name, ":clear") == 0) {
+    }
+    else if (strcmp(name, ":clear") == 0)
+    {
         BeginTextureMode(config->canvas);
-            ClearBackground(DARKGRAY);
+        ClearBackground(DARKGRAY);
         EndTextureMode();
-    } else if (strcmp(name, ":help") == 0) {
+    }
+    else if (strcmp(name, ":help") == 0)
+    {
         // TODO
     }
 }
 
-int main() {
+int main()
+{
     Config config = {0};
     InitConfig(&config);
 
     BeginTextureMode(config.canvas);
-        ClearBackground(DARKGRAY);
+    ClearBackground(DARKGRAY);
     EndTextureMode();
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         // draw
         BeginTextureMode(config.canvas);
-            config.curr_pos = GetMousePosition();
-            if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) || IsKeyDown(KEY_LEFT_SUPER)) {
-                DrawLineVec(config.curr_pos, config.prev_pos, config.brush_size, WHITE);
-            } else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) || IsKeyDown(KEY_RIGHT_SUPER)) {
-                DrawLineVec(config.curr_pos, config.prev_pos, config.brush_size, DARKGRAY);
-            }
-            config.prev_pos = config.curr_pos;
+        config.curr_pos = GetMousePosition();
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) || IsKeyDown(KEY_LEFT_SUPER))
+        {
+            DrawLineVec(config.curr_pos, config.prev_pos, config.brush_size, BRUSH_COLOR);
+        }
+        else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) || IsKeyDown(KEY_RIGHT_SUPER))
+        {
+            DrawLineVec(config.curr_pos, config.prev_pos, config.brush_size, BACKGROUND_COLOR);
+        }
+        config.prev_pos = config.curr_pos;
         EndTextureMode();
 
         // commands
-        if (IsKeyPressed(KEY_SEMICOLON) && IsKeyDown(KEY_LEFT_SHIFT)) {
+        if (IsKeyPressed(KEY_SEMICOLON) && IsKeyDown(KEY_LEFT_SHIFT))
+        {
             config.command_mode = true;
         }
 
-        if (config.command_mode) {
+        if (config.command_mode)
+        {
             int ch = GetCharPressed();
 
             // if user pressed multiple times during one frame
-            while (ch > 0) {
-                if (config.cmd_idx < CMD_CAP - 1) {
+            while (ch > 0)
+            {
+                if (config.cmd_idx < CMD_CAP - 1)
+                {
                     config.cmd[config.cmd_idx++] = ch;
                     config.cmd[config.cmd_idx] = '\0';
                 }
                 ch = GetCharPressed();
             }
 
-            if (IsKeyPressed(KEY_BACKSPACE)) {
-                if (config.cmd_idx != 0) {
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                if (config.cmd_idx != 0)
+                {
                     config.cmd[--config.cmd_idx] = '\0';
                 }
             }
 
-            if (IsKeyPressed(KEY_ENTER) || config.cmd_idx == 0) {
+            if (IsKeyPressed(KEY_ENTER) || config.cmd_idx == 0)
+            {
                 execute_command(&config, config.cmd);
                 config.command_mode = false;
                 config.cmd_idx = 0;
@@ -153,12 +182,13 @@ int main() {
 
         // copy canvas to window
         BeginDrawing();
-            DrawTextureRec(config.canvas.texture, (Rectangle){0, 0, (float)config.canvas.texture.width, (float)-config.canvas.texture.height}, (Vector2){0, 0}, GRAY);
+        DrawTextureRec(config.canvas.texture, (Rectangle){0, 0, (float)config.canvas.texture.width, (float)-config.canvas.texture.height}, (Vector2){0, 0}, GRAY);
 
-            if (config.command_mode) {
-                DrawRectangle(CMD_LINE_X, CMD_LINE_Y, CMD_LINE_W, CMD_LINE_H, LIGHTGRAY);
-                DrawText(config.cmd, CMD_LINE_X * 0.9f, CMD_LINE_Y + CMD_LINE_H / 2 - 40, 80, WHITE);
-            }
+        if (config.command_mode)
+        {
+            DrawRectangle(0, CMD_LINE_Y, CMD_LINE_W, CMD_LINE_H, CMD_LINE_COLOR);
+            DrawText(config.cmd, FONT_SIZE, CMD_LINE_Y + CMD_LINE_H / 2 - FONT_SIZE / 2, FONT_SIZE, FONT_COLOR);
+        }
         EndDrawing();
     }
 
